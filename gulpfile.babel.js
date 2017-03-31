e8895f1766a6e9337e46e4f770d0fea176a8411d
Junbang Huang <wongzeonbong@gmail.com>
2017-03-31 16:42:05 -0400

    update name

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,14 +4,14 @@ import gulp from "gulp";
 import babel from "gulp-babel";
 import jasmine from "gulp-jasmine";
 
-const build = () => gulp.src(["rbgt-api.js", "test/test.js"]).pipe(babel()).pipe(gulp.dest(build));
+const build = () => gulp.src(["tpb-api.js", "test/test.js"]).pipe(babel()).pipe(gulp.dest(build));
 const test = () => gulp.src("test/test.js").pipe(jasmine());
 
 gulp.task("clean", () => del(["build"]));
 
 gulp.task("build", ["clean"], build);
 
-gulp.task("watch", ["build"], () => gulp.watch("rbgt-api.js", ["build"]));
+gulp.task("watch", ["build"], () => gulp.watch("tpb-api.js", ["build"]));
 
 gulp.task("default", ["clean"], build);
 
